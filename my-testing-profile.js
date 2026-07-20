a12ab211a80b2af6861791d21e6c0b9a27c42e95
swapnilkulkarni13721-cmd <swapnil.kulkarni13721@gmail.com>
2026-07-20 13:07:38 +0530

    Delete my-testing-profile.js

--- my-testing-profile.js
+++
@@ -1,8 +0,0 @@
-//Part-1
-console.log("============My Testing Profile============");
-console.log("Name : Swapnil");
-console.log("Role : Automation tester");
-console.log("Experience : 10 years");
-console.log("Favorite Tool : JIRA");
-console.log("Learning Goal : To become a good Automation tester");
-
